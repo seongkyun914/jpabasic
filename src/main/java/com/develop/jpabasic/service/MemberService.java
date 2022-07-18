68243955b34945e9b364c3390b7637a212ffd93f
seongkyun <gyun59@gmail.com>
2022-07-18 16:10:05 +0900

    Initial commit

--- src/main/java/com/develop/jpabasic/service/MemberService.java
+++ src/main/java/com/develop/jpabasic/service/MemberService.java
@@ -3,7 +3,6 @@ package com.develop.jpabasic.service;
 import com.develop.jpabasic.domain.Member;
 import com.develop.jpabasic.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
